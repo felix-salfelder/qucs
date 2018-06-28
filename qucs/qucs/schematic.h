d7f70904ff27dccbf2df66b91b9331d8363656f3
Guilherme Brondani Torri <guitorri@gmail.com>
2018-06-29 00:18:02 +0200

    Remove selectedComponent
    Same effect can be done with itemAt

--- qucs/qucs/schematic.h
+++ qucs/qucs/schematic.h
@@ -241,7 +241,6 @@ public:
   void       setCompPorts(Component*);
   Component* selectCompText(int, int, int&, int&);
   Component* searchSelSubcircuit();
-  Component* selectedComponent(int, int);
   void       deleteComp(Component*);
 
   void     oneLabel(Node*);
