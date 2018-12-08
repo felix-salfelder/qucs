52d8e34261b84df1e2d56c6acafbc57bddfeedec
Felix Salfelder <felix@salfelder.org>
2018-12-08 00:35:17 +0000

    rearrange the build a bit. now loading/registering is working
    - one more library
    - switch to shared private libraries
    - use libtool

--- qucs/qucs/globals.cpp
+++ qucs/qucs/globals.cpp
@@ -1,20 +1,4 @@
-/***************************************************************************
-                              globals.h
-                             --------------------
-    author               : 2018 Felix Salfelder
-    license              : GPLv3+
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-#include "globals.h"
+#include "l_dispatcher.h"
+#include "symbol.h"
 
 Dispatcher<Symbol> symbol_dispatcher;
-Dispatcher<Simulator> simulator_dispatcher;
-Dispatcher<NetLang> netlang_dispatcher;
